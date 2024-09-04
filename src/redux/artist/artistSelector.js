4d9904e1c761dc5df53516841643fe5a261f8fdd
Format_Pro <lotfi.djeg@gmail.com>
2024-09-04 14:55:48 +0200

    Refresh

--- src/redux/artist/artistSelector.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from "@reduxjs/toolkit";
-
-const selectLoading = state => state.artists.loading;
-const selectArtist = state => state.artists.artistDetail;
-
-export const selectArtistData = createSelector(
-    [selectLoading, selectArtist], 
-    (loading, artistDetail) => ({ loading, artistDetail })
-)
